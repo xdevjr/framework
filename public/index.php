34ed313a9c3dc2fd0668d306980b2500a2ddb0ae
JuniorGS <josival1998@gmail.com>
2024-04-21 03:33:56 -0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+use Pecee\SimpleRouter\SimpleRouter as Router;
+
+require ("../vendor/autoload.php");
+
+Router::setDefaultNamespace("app\\controllers");
+Router::get("/", "HomeController@index");
+
+Router::start();
