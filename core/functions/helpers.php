27ede6223ceec5faf6b254f8e4be159fffa8f258
JuniorGS <josival1998@gmail.com>
2024-04-25 19:34:35 -0300

    new helper

--- core/functions/helpers.php
+++ core/functions/helpers.php
@@ -1,11 +1,17 @@
 <?php
-use core\library\Request;
-use Pecee\SimpleRouter\SimpleRouter as Router;
 use Pecee\Http\Url;
+use core\library\Request;
+use core\library\Response;
+use Pecee\SimpleRouter\SimpleRouter as Router;
 
 function request(): Request
 {
     return Request::all();
+}
+
+function response(mixed $body, int $statusCode = 200, array $headers = []): Response
+{
+    return new Response($body, $statusCode, $headers);
 }
 
 function url(?string $name = null, $parameters = null, ?array $getParams = null): Url
