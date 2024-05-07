c2c4292c276886212d4b500216705a4f1caf67c7
JuniorGS <josival1998@gmail.com>
2024-05-07 03:53:16 -0300

    remove pecee/simple-router dependencie

--- public/index.php
+++ public/index.php
@@ -5,14 +5,7 @@ use app\middlewares\Auth;
 use app\middlewares\Teste;
 use core\library\router\Router;
 
-// use Pecee\SimpleRouter\SimpleRouter as Route;
-
 require ("../vendor/autoload.php");
-
-// Route::setDefaultNamespace("app\\controllers");
-// Route::get("/{page?}", "HomeController@index");
-
-// Route::start();
 
 $router = new Router("app\\controllers\\");
 
